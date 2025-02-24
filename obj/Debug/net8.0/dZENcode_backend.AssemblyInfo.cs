8bf66d16499d93ffe55e1160d4b8ed6587a6d759
Ivan Ushachov <ushachovg324@gmail.com>
2025-02-24 12:45:17 +0200

    updates

--- obj/Debug/net8.0/dZENcode_backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/dZENcode_backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dZENcode_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f421e6d73af397609fe43a83a81e8dabdeceecd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a7b3a7c25ca529ab8f2fd6559c9820baa9b074")]
 [assembly: System.Reflection.AssemblyProductAttribute("dZENcode_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dZENcode_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
